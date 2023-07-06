57c2cdf15b6e731c8039b9af13e8bfb9e4fcc9ab
Adrian Castillo <adrianangulo1080@gmail.com>
2023-07-06 00:20:26 -0500

    primer commit

---
+++ src/com/platzi/mensajes_app/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.platzi.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+/**
+ *
+ * @author ADRIAN CASTILLO
+ */
+public class Conexion {
+    public Connection get_Connection(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+            if(con != null){
+                System.out.println("conexion existosa");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return con;
+    }
+}
---
+++ src/com/platzi/mensajes_app/Inicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.platzi.mensajes_app;
+import java.sql.Connection;
+/**
+ *
+ * @author ADRIAN CASTILLO
+ */
+public class Inicio {
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        try (Connection cnx = conexion.get_Connection()){
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
